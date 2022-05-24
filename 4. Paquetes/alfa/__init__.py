ae47f63244b7e492086ed5077e6c872e6506bf9d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-05-24 23:09:36 +0000

    Agrega archivos y carpetas para manejo de paquetes

---
+++ 4. Paquetes/alfa/__init__.py
@@ -0,0 +1,1 @@
+print('inicializando alfa...')
---
+++ 4. Paquetes/main.py
@@ -0,0 +1,10 @@
+from alfa.uno import fun_uno, Uno
+from alfa.beta.dos import fun_dos
+import gamma.tres as firulais
+
+fun_uno()
+u = Uno()
+print(u)
+fun_dos()
+t = firulais.Tres()
+print(t)
